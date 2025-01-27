df2406e3d0b654c716a7f4f88188c15485e8f063
Edward Joshua Diesta <99942301+DrawdEA@users.noreply.github.com>
2025-01-27 15:54:59 +0800

    Finish image carousel time interval

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import "./styles.css";
 
-/* Dropdown logic */
+// Dropdown logic
 const dropdownButton = document.querySelector(".dropdown-button");
 const dropdownSelection = document.querySelector(".dropdown > div");
 
 function createDropdown(button, selection) {
     button.addEventListener("click", () => {
+        console.log("HA");
         selection.style.display = selection.style.display === "flex" ? "none" : "flex";
         
     })
@@ -13,13 +14,36 @@ function createDropdown(button, selection) {
 
 createDropdown(dropdownButton, dropdownSelection);
 
-/* Image carousel logic */
+// Image carousel logic
+
+// Scrolls to an image immediately
+function scrollToNextImage() {
+    const slides = document.querySelector(".carousel").closest("[data-carousel]").querySelector("[data-slides]");
+    const navigationMain = document.querySelector(".navigation")
+    const activeNavigation = navigationMain.querySelector("[data-active]");
+
+    const activefSlide = slides.querySelector("[data-active]");
+    let newIndex = [...slides.children].indexOf(activefSlide) + 1;
+    if (newIndex < 0) newIndex = slides.children.length - 1;
+    if (newIndex >= slides.children.length) newIndex = 0;
+
+    slides.children[newIndex].dataset.active = true;
+    delete activefSlide.dataset.active;
+
+    navigationMain.children[newIndex].dataset.active = true;
+    delete activeNavigation.dataset.active;
+}
+
+let timer = setInterval(scrollToNextImage, 5000);
+
 const buttons = document.querySelectorAll("[data-carousel-button]");
 
 buttons.forEach(button => {
     button.addEventListener("click", () => {
        const offset = button.dataset.carouselButton === "next" ? 1 : -1;
        const slides = button.closest("[data-carousel]").querySelector("[data-slides]");
+       const navigationMain = document.querySelector(".navigation")
+       const activeNavigation = navigationMain.querySelector("[data-active]");
 
         const activefSlide = slides.querySelector("[data-active]");
         let newIndex = [...slides.children].indexOf(activefSlide) + offset;
@@ -28,6 +52,12 @@ buttons.forEach(button => {
 
         slides.children[newIndex].dataset.active = true;
         delete activefSlide.dataset.active;
+
+        navigationMain.children[newIndex].dataset.active = true;
+        delete activeNavigation.dataset.active;
+
+        clearInterval(timer)
+        timer = setInterval(scrollToNextImage, 5000);
     })
 })
 
@@ -37,20 +67,15 @@ navigations.forEach(navigation => {
         const slides = navigation.closest("[data-carousel]").querySelector("[data-slides]");
         const index = [...navigations].indexOf(navigation);
         const activefSlide = slides.querySelector("[data-active]");
-        
-        const activeNavigation = document.querySelector(".navigation").querySelector("[data-active]");
+        const navigationMain = document.querySelector(".navigation")
+        const activeNavigation = navigationMain.querySelector("[data-active]");
         
         // Set slide
         slides.children[index].dataset.active = true;
         delete activefSlide.dataset.active;
 
         // Set button visual
-        navigations.children[index].dataset.active = true;
+        navigationMain.children[index].dataset.active = true;
         delete activeNavigation.dataset.active;
     })
 })
-
-
-setInterval(() => {
-    // TODO: Add automatic thing
-}, 5000)
